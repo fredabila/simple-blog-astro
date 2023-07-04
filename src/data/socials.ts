ab92b8162d13bcbe49187912ed3d8a1e72daf937
frederickabila <sharksinternet@gmail.com>
2023-07-04 17:30:00 +0000

    new

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,17 +1,3 @@
 export const socials = [
-  {
-    name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
-    icon: 'mdi:twitter'
-  },
-  {
-    name: 'GitHub',
-    url: 'https://github.com/littlesticks',
-    icon: 'mdi:github'
-  },
-  {
-    name: 'Instagram',
-    url: 'https://instagram.com',
-    icon: 'mdi:instagram'
-  }
+
 ]
